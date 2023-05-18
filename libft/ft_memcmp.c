7972d0fa03027e5d78d4247e38a1c9243d3dcdb2
Mathieu HOYER <mathieuhoyer@gmail.com>
2023-05-18 10:33:43 +0200

    makefilebien

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -14,19 +14,19 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned int		i;
-	unsigned char		*str1;
-	unsigned char		*str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while (i < n - 1 && str1[i] == str2[i])
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
-	if (str1[i] - str2[i] > 0 && (int)n >= 0)
-		return (n);
-	else if (str1[i] - str2[i] < 0 && (int)n >= 0)
-		return (-1);
+	}
 	return (0);
 }
 
@@ -35,7 +35,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 #include <string.h>
 int main()
 {
-	printf("%d\n", ft_memcmp("RD6Ygh1T2wePOcv", "dinHcY ", -1));
-	printf("%d\n", memcmp("RD6Ygh1T2wePOcv", "dinHcY ", -1));
+	printf("%d\n", ft_memcmp("RD6Ygh1T2wePOcv", "RD6YainHcY ", 5));
+	printf("%d\n", memcmp("RD6Ygh1T2wePOcv", "RD6YainHcY ", 5));
 	return 0;
 }*/
